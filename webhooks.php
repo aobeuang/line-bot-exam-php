cfc4efe8b22aaae838dd2966056cc72688198ba4
aobeuang <aob.attaporn@gmail.com>
2019-05-22 22:29:51 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -37,7 +37,7 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 	    }
 	    #ตัวอย่าง Message Type "Image"
 	    else if(strpos($message, 'รูปน้องแมว') !== false){
-	        $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+	        $image_url = "https://loremflickr.com/300/200/cat";
 	        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
 	        $arrayPostData['messages'][0]['type'] = "image";
 	        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;

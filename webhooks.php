3ccf853710ee01da3008a9f626ff30229e5589a9
aobeuang <aob.attaporn@gmail.com>
2019-05-23 07:13:49 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -101,6 +101,7 @@ $baseurl = "https://" . $_SERVER['SERVER_NAME'];
     if(strpos($message, 'เทส') !== false){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0] = gProfile($arrayJson['events'][0]['source']['userId']);
+        echo gProfile($arrayJson['events'][0]['source']['userId']);
         replyMsg($arrayHeader,$arrayPostData);
     }
 

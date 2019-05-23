921425bf81091b6a397931a0fba400f4341ad6e7
aobeuang <aob.attaporn@gmail.com>
2019-05-23 07:00:45 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,11 +1,12 @@
 <?php
 
+function gUserdetail($userid)
+{
+	# code...
 
 $access_token = 'xBbo2kYd5sOVIQ1f082vd75ZKZtHdXcZ7OnRyb/Q1+HGhgZhKWiWFZwM8wSZwCQhNLwhfA1tq+65ifktQKg4D3W94GA1oKhS8vxAgKK3jROA7ec+iu4IV0xcJZTUiZh9l0ch6Zx4CTwKSzfKDTfOnwdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'U6e89ee65c131d0f0db2a0b1644a319c8';
-
-$url = 'https://api.line.me/v2/bot/profile/'.$userId;
+$url = 'https://api.line.me/v2/bot/profile/'.$userid;
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
@@ -16,32 +17,11 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 
-$ss = json_decode($result);
-
-
 echo $ss->userId;
 echo $ss->displayName;
 echo $ss->pictureUrl;
 
-// function gUserdetail($userid)
-// {
-// 	# code...
 
-// $access_token = 'xBbo2kYd5sOVIQ1f082vd75ZKZtHdXcZ7OnRyb/Q1+HGhgZhKWiWFZwM8wSZwCQhNLwhfA1tq+65ifktQKg4D3W94GA1oKhS8vxAgKK3jROA7ec+iu4IV0xcJZTUiZh9l0ch6Zx4CTwKSzfKDTfOnwdB04t89/1O/w1cDnyilFU=';
-
-// $url = 'https://api.line.me/v2/bot/profile/'.$userid;
-
-// $headers = array('Authorization: Bearer ' . $access_token);
-
-// $ch = curl_init($url);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-// $result = curl_exec($ch);
-// curl_close($ch);
-
-// return $result;
-
-// }
+}
 
 ?>

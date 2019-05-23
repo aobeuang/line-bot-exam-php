402b2590db64151c48e7a2b8424128448e7982b8
aobeuang <aob.attaporn@gmail.com>
2019-05-23 07:37:50 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -159,7 +159,7 @@ function gProfile($userid)
 						        2 => 
 						        array (
 						          'type' => 'text',
-						          'text' => ( isset($userss->statusMessage) ? $userss->statusMessage : ''),
+						          'text' => $userss->statusMessage,
 						          'align' => 'center',
 						          'gravity' => 'center',
 						          'weight' => 'regular',
